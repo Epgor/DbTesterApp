cf2a60cc8df64665d46822a4a79a1be5ada0b8ad
Epgor <61190141+Epgor@users.noreply.github.com>
2023-01-07 14:14:50 +0100

    naming change

--- DbTesterApp/Controllers/BookRedisController.cs
+++ DbTesterApp/Controllers/BookRedisController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using StackExchange.Redis;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class BookRedisController : ControllerBase
 {
 
